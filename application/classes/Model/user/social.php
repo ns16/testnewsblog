c728bd26d5d5c441edb780515b1dd84d8d5ed3ba
Nikolay Shamayko <nikolay.shamayko@gmail.com>
2016-03-11 19:27:16 +0600

    Change edit of settings of social networks

--- application/classes/Model/user/social.php
+++ application/classes/Model/user/social.php
@@ -2,7 +2,37 @@
 
 class Model_User_Social extends ORM {
 
-    protected $_table_name = 'user_social';
+    protected $_table_name  = 'user_social';
     protected $_primary_key = 'user_id';
 
+    public function rules()
+    {
+        return array(
+            TRUE => array(
+                array('max_length', array(':value', 255)),
+                array('Valid::profile', array(':value', ':field')),
+            ),
+        );
+    }
+
+    public function filters()
+    {
+        return array(
+            TRUE => array(
+                array('trim'),
+                array('strip_tags'),
+            ),
+        );
+    }
+
+    public function labels()
+    {
+        return array(
+            'profile_vk' => 'Вконтакте',
+            'profile_fb' => 'Facebook',
+            'profile_gp' => 'Google+',
+            'profile_tw' => 'Twitter',
+            'profile_ok' => 'Одноклассники',
+        );
+    }
 }

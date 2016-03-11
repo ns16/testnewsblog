ee50806a1d60d59630a1a0fa4c44307d4770b5de
Nikolay Shamayko <nikolay.shamayko@gmail.com>
2016-03-11 17:10:46 +0600

    Add URL::get_default_url()

--- application/classes/URL.php
+++ application/classes/URL.php
@@ -2,6 +2,17 @@
 
 class URL extends Kohana_URL {
 
+    public static function get_default_url($controller = '', $action = '', $id = NULL)
+    {
+        return Route::url('default',
+            array(
+                'controller' => $controller,
+                'action'     => $action,
+                'id'	     => $id,
+            )
+        );
+    }
+
     public static function get_current_url()
     {
         return URL::base(TRUE).Request::current()->uri();

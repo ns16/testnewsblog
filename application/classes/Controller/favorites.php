66f9f64d1c478cd170e52d09d98da78766208604
Nikolay Shamayko <nikolay.shamayko@gmail.com>
2016-03-23 17:16:55 +0600

    Started to make AJAX for favorite articles

--- application/classes/Controller/favorites.php
+++ application/classes/Controller/favorites.php
@@ -4,6 +4,8 @@ class Controller_Favorites extends Controller
 {
     protected $user_id    = NULL;
     protected $article_id = NULL;
+    protected $message    = NULL;
+    protected $class      = NULL;
 
     /**
      * Данный экшен после добавления статьи в избранное или удаления ее из
@@ -12,13 +14,31 @@ class Controller_Favorites extends Controller
      */
     public function action_index()
     {
-        $this->toggle();
+        if ($this->is_ajax())
+        {
+            $post = $this->request->post();
 
-        $this->redirect(URL::get_default_url(
-            'articles',
-            '',
-            $this->article_id
-        ));
+            $this->user_id = Arr::get($post, 'user_id');
+            $this->article_id = Arr::get($post, 'article_id');
+
+            $this->toggle();
+
+
+
+            $json = array(
+                'message' => $this->message,
+                'class'   => $this->class,
+            );
+            $jsonstring = json_encode($json);
+            echo $jsonstring;
+        }
+
+
+//        $this->redirect(URL::get_default_url(
+//            'articles',
+//            '',
+//            $this->article_id
+//        ));
     }
 
     /**
@@ -30,20 +50,24 @@ class Controller_Favorites extends Controller
     protected function toggle()
     {
         // Get id of current user
-        $user = Auth::instance()->get_user();
-        $this->user_id = isset($user) ? $user->id : NULL;
+//        $user = Auth::instance()->get_user();
+//        $this->user_id = isset($user) ? $user->id : NULL;
 
         // Get id of article
-        $this->article_id = $this->request->param('id');
+//        $this->article_id = $this->request->param('id');
 
         // If user isn't logged, then reload page
         if ( ! $this->user_id)
         {
-            $this->redirect(URL::get_default_url(
-                'articles',
-                '',
-                $this->article_id
-            ));
+//            $this->redirect(URL::get_default_url(
+//                'articles',
+//                '',
+//                $this->article_id
+//            ));
+            $this->message = 'Авторизуйтесь или зарегистрируйтесь, чтобы добавить статью в избранное!';
+            $this->class   = '';
+
+            return;
         }
 
         // Get model of link of user and article with given ids
@@ -56,6 +80,9 @@ class Controller_Favorites extends Controller
         {
             // If model is loaded, then delete record from table
             $model->delete();
+
+            $this->message = 'Статья удалена из избранного';
+            $this->class   = 'glyphicon-star glyphicon-star-empty';
         }
         else
         {
@@ -66,7 +93,17 @@ class Controller_Favorites extends Controller
                     'article_id' => $this->article_id,
                 ))
                 ->save();
+
+            $this->message = 'Статья добавлена в избранное';
+            $this->class   = 'glyphicon-star glyphicon-star-empty';
         }
+    }
+
+    protected function is_ajax()
+    {
+        return isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+            AND ! empty($_SERVER['HTTP_X_REQUESTED_WITH'])
+            AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
     }
 
 } // End Favorites
--- application/views/widget/favorite.php
+++ application/views/widget/favorite.php
@@ -1,5 +1,6 @@
-<span id="content-article-title-favorite">
-    <a href="<?= URL::get_default_url('favorites', 'index', $article_id); ?>" class="btn btn-default">
+<div id="article-title-favorite">
+    <!--<a href="<?/*= URL::get_default_url('favorites', 'index', $article_id); */?>" class="btn btn-default">-->
+    <a id="article-title-favorite-button" class="btn btn-default">
         <span class="glyphicon
         <? if (in_array($current_user_id, $user_ids)): ?>
             glyphicon-star
@@ -8,4 +9,39 @@
         <? endif; ?>
         "></span>
     </a>
-</span>
+</div>
+<script>
+
+    var button = document.getElementById("article-title-favorite-button");
+    var span   = button.firstElementChild;
+
+    var article_id = <?= $article_id; ?>;
+    var user_id    = <?= isset($current_user_id) ? $current_user_id : 'null'; ?>;
+
+    button.onclick = function()
+    {
+        toggle_favorite();
+    }
+
+    function toggle_favorite()
+    {
+        $.ajax({
+            url: "<?= URL::get_default_url('favorites', 'index'); ?>",
+            type: "POST",
+            data: {
+                article_id: article_id,
+                user_id: user_id
+            },
+            dataType: "json"
+        })
+        .done(function(json) {
+
+//            span.classList.toggle(json.class);
+
+            $(span).toggleClass(json.class);
+            alert(json.message);
+
+        });
+    }
+
+</script>
